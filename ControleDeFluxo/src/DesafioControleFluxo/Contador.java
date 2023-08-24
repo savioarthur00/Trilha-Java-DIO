6750e3f8a2f7fd1210727c211328e11854dd2f70
savioarthur00 <savio1281@gmail.com>
2023-08-24 10:02:49 -0300

    Adicionar pasta ControleDeFluxo

---
+++ ControleDeFluxo/src/DesafioControleFluxo/Contador.java
@@ -0,0 +1,41 @@
+package DesafioControleFluxo;
+
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("Erro: " + exception.getMessage());
+        }
+        terminal.close();
+    }
+
+    static class ParametrosInvalidosException extends Exception {
+       
+		private static final long serialVersionUID = 1L;
+
+		public ParametrosInvalidosException(String message) {
+            super(message);
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O parâmetro 2 deve ser maior que o parâmetro 1");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 0; i <= contagem; i++) {
+            System.out.println(parametroUm + i);
+        }
+    }
+}
